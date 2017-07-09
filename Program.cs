796d593470e3a2f3aa863569de05416106236e18
Hunter <hunter.barclay18@gmail.com>
2017-07-09 03:34:27 -0700

    Main C-Sharp Scripts
    Why am I uploading this at 3:30 AM???
    * Because your not human, Hunter *

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Willy.Core;
+using Willy.res;
+
+namespace Willy
+{
+    class Program
+    {
+        //'Logging in' Sort of speak
+        static void Main(string[] args)
+        {
+            Handler hand = new Handler();
+            WilliamsAspects _mr;
+            hand.Williams_Aspects.TryGetValue("my reference", out _mr);
+            MyReference mr = (MyReference)_mr;
+            Console.Write(" > ");
+            for (int i = 0; i <= hand.MaxAttempts; i++) {
+                string a = Console.ReadLine();
+                if (mr.isGreetingAccepted(a))
+                {
+                    break;
+                }
+                else if (i == hand.MaxAttempts)
+                {
+                    Console.WriteLine(hand.AnnomTitle + " > I recommend you speak with Mr. Barclay...");
+                    Console.WriteLine("---Application Abort Requested---");
+                    Console.ReadKey();
+                    return;
+                } else
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            Console.WriteLine(hand.AnnomTitle + " > I'm Sorry?");
+                            Console.Write(" > ");
+                            break;
+                        case 1:
+                            Console.WriteLine(hand.AnnomTitle + " > Pardon?");
+                            Console.Write(" > ");
+                            break;
+                        case 2:
+                            Console.WriteLine(hand.AnnomTitle + " > What?");
+                            Console.Write(" > ");
+                            break;
+                        default:
+                            Console.WriteLine(hand.AnnomTitle + " > I don't Understand that.");
+                            Console.Write(" > ");
+                            break;
+                    }
+                }
+            }
+            WilliamsAspects wa;
+            hand.Williams_Aspects.TryGetValue("startup", out wa);
+            StartUp Su = (StartUp)wa;
+            hand.WilliamsOutput(Su.WilliamsGreeting());
+            hand.Williams_Aspects.TryGetValue("core", out wa);
+            Willy.Core.Core c = (Willy.Core.Core)wa;
+            c.Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SystemUse.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Willy.Core;
+
+namespace Willy
+{
+    class SystemUse : WilliamsAspects
+    {
+        public interface Accessable
+        {
+            void Use();
+        }
+
+        public static string[] Objectives = new string[]
+        {
+            "greeting",
+            "greetings",
+            "message",
+            "william"
+        };
+
+        public static object[] AddArray(object[] current, object addition)
+        {
+            object[] a = new object[current.Length + 1];
+            for (int i = 0; i < current.Length; i++)
+            {
+                a[i] = current[i];
+            }
+            a[a.Length - 1] = addition;
+            return a;
+        }
+
+        public static string[] AddString(string[] current, string addition)
+        {
+            string[] a = new string[current.Length + 1];
+            for (int i = 0; i < current.Length; i++)
+            {
+                a[i] = current[i];
+            }
+            a[a.Length - 1] = addition;
+            return a;
+        }
+
+        public static string FindObjective(string[] words)
+        {
+            for (int x = 0; x < words.Length; x++)
+            {
+                for (int y = 0; y < Objectives.Length; y++)
+                {
+                    if (words[x].ToLower() == Objectives[y].ToLower())
+                        return words[x];
+                }
+            }
+
+            return "error";
+        }
+
+        public static Action FindAction(string[] words, Dictionary<string, Action> Actions)
+        {
+            try
+            {
+                for (int x = 0; x < words.Length; x++)
+                {
+                    Action ob;
+                    Actions.TryGetValue(words[x], out ob);
+                    if (ob != null)
+                    {
+                        return ob;
+                    }
+                }
+
+                return null;
+            } catch (Exception e)
+            {
+                Handler.Inst.WilliamsOutput("Exception(s) Where Unfortunately Thrown:\n" + e.StackTrace);
+                return null;
+            }
+        }
+
+        public static string[] BreakDownSentence(string line)
+        {
+            string[] a = new string[0];
+            string current = "";
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '.' || line[i] == ',' || line[i] == '?' || line[i] == '!' || line[i] == ' ')
+                {
+                    a = AddString(a, current);
+                    current = "";
+                } else
+                {
+                    current += line[i];
+                }
+            }
+
+            a = AddString(a, current);
+            current = "";
+
+            return a;
+        }
+    }
+}
